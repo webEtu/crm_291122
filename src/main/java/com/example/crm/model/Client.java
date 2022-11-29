9cbef1981c13f380265dbdec6436eff4df8969d3
webEtu <azadeh.ghasemizadeh2022@campus-eni.fr>
2022-11-29 14:48:02 +0100

    add getter and setter

--- src/main/java/com/example/crm/model/Client.java
+++ src/main/java/com/example/crm/model/Client.java
@@ -19,4 +19,112 @@ public class Client {
     private String country;
     private boolean state;
 
+    public Client() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCompany_name() {
+        return company_name;
+    }
+
+    public void setCompany_name(String company_name) {
+        this.company_name = company_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZip_code() {
+        return zip_code;
+    }
+
+    public void setZip_code(String zip_code) {
+        this.zip_code = zip_code;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", company_name='" + company_name + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", zip_code='" + zip_code + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", state=" + state +
+                '}';
+    }
 }
--- src/main/java/com/example/crm/model/Order.java
+++ src/main/java/com/example/crm/model/Order.java
@@ -18,4 +18,93 @@ public class Order {
     private float total_exclude_taxe;
     private float total_with_taxe;
 
+    public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getType_presta() {
+        return type_presta;
+    }
+
+    public void setType_presta(String type_presta) {
+        this.type_presta = type_presta;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getNb_days() {
+        return nb_days;
+    }
+
+    public void setNb_days(int nb_days) {
+        this.nb_days = nb_days;
+    }
+
+    public float getUnit_price() {
+        return unit_price;
+    }
+
+    public void setUnit_price(float unit_price) {
+        this.unit_price = unit_price;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public float getTotal_exclude_taxe() {
+        return total_exclude_taxe;
+    }
+
+    public void setTotal_exclude_taxe(float total_exclude_taxe) {
+        this.total_exclude_taxe = total_exclude_taxe;
+    }
+
+    public float getTotal_with_taxe() {
+        return total_with_taxe;
+    }
+
+    public void setTotal_with_taxe(float total_with_taxe) {
+        this.total_with_taxe = total_with_taxe;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", client=" + client +
+                ", type_presta='" + type_presta + '\'' +
+                ", designation='" + designation + '\'' +
+                ", nb_days=" + nb_days +
+                ", unit_price=" + unit_price +
+                ", state=" + state +
+                ", total_exclude_taxe=" + total_exclude_taxe +
+                ", total_with_taxe=" + total_with_taxe +
+                '}';
+    }
 }
