8f530011a9a6ac9639d8e161815fed315c3d62d8
webEtu <azadeh.ghasemizadeh2022@campus-eni.fr>
2022-11-30 07:07:35 +0100

    add method getClientById to ClientDirectory and ClientController

--- src/main/java/com/example/crm/controller/ClientController.java
+++ src/main/java/com/example/crm/controller/ClientController.java
@@ -1,23 +1,38 @@
 package com.example.crm.controller;
 
 import com.example.crm.model.Client;
+import com.example.crm.service.ClientDirectory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api")
 public class ClientController {
 
     @Autowired
-    private ClientController clientController;
+    private ClientDirectory  clientDirectory;
 
     @GetMapping("clients")
     public List<Client> getClients() {
-        return clientController.getClients();
+        return clientDirectory.getClients();
     }
+
+    @GetMapping("clients/{id}")
+    public ResponseEntity<Client> getClient(@PathVariable("id") Integer id) {
+        Optional<Client> optionalClient = clientDirectory.getClient(id);
+        if(optionalClient.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(optionalClient.get());
+        }
+    }
+
 
 }
--- src/main/java/com/example/crm/service/ClientDirectory.java
+++ src/main/java/com/example/crm/service/ClientDirectory.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientDirectory {
@@ -20,6 +21,10 @@ public class ClientDirectory {
 
     public List<Client> getClients() {
         return clientRepository.findAll();
+    }
+
+    public Optional<Client> getClient(Integer id) {
+        return clientRepository.findById(id);
     }
 
 
